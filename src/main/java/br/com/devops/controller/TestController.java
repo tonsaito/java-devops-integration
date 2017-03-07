6489ecaf07dc8e635766633db3681e92d8cbfa22
Ayrton Saito <ton.unesp@gmail.com>
2017-03-07 00:37:26 -0300

    Auto Commit by Jenkins

--- src/main/java/br/com/devops/controller/TestController.java
+++ src/main/java/br/com/devops/controller/TestController.java
@@ -3,7 +3,6 @@ package br.com.devops.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController { 
@@ -12,13 +11,6 @@ public class TestController {
 	public String test(Model model){
 		model.addAttribute("message", "Maven + Spring Up and Running");
 		return "test/index";
-	}
-	
-	@RequestMapping("/test2")
-	public ModelAndView test(){
-		ModelAndView modelView = new ModelAndView("test/index");
-		modelView.addObject("message", "Maven + Spring Up and Running");
-		return modelView;
 	}
 	
 }
